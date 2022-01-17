2273734759bebffa92c2f5fe20cbaf9f07b17849
Josué Merino <yoshichicharra11@gmail.com>
2022-01-17 18:25:55 -0500

    Primera Parte Proyecto

---
+++ Polaca Inversa/PolacaInversa.cpp
@@ -0,0 +1,20 @@
+/*
+	Universidad de las Fuerzas Armadas - ESPE
+	Proyecto - Segundo Parcial - Calculadora Polaca Inversa
+	Autores:
+	Paúl Mena
+	Josué Merino
+	Daniel Vizcarra
+	Fecha de Creación: 15/01/2022
+	Fecha de Modificación: 17/01/2022
+*/
+
+#include <iostream>
+#include "Stack.h"
+#include "Operation.h"
+#include "Menu.h"
+using namespace std;
+int main()
+{
+    menu_principal();
+}
